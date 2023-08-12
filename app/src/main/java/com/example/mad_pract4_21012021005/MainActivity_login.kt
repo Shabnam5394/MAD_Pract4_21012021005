38a5a12cbc800d20fe5b0d3fc1e4ad23a92e6aaa
Ansari Shabnam <ansarimuskan5394@gmail.com>
2023-08-12 15:01:12 +0530

    .

--- app/src/main/java/com/example/mad_pract4_21012021005/MainActivity_login.kt
+++ app/src/main/java/com/example/mad_pract4_21012021005/MainActivity_login.kt
@@ -1,11 +1,19 @@
 package com.example.mad_pract4_21012021005
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity_login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_login)
+
+        val button_login=findViewById<Button>(R.id.button_login)
+        button_login.setOnClickListener{
+            val intent3=Intent(this,MainActivity_register::class.java)
+            startActivity(intent3)
+        }
     }
 }
